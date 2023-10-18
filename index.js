4283ee7f30c1df7328138e1060cff5d38ab52cb5
rayhan360 <hossainrayhan897@gmail.com>
2023-10-19 01:56:36 +0600

    added deleted operation /cart section route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -63,6 +63,14 @@ async function run() {
         const result = await cartCollection.insertOne(newCart)
         res.send(result)
     })
+
+    // delete a cart data
+    app.delete('/cart/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await cartCollection.deleteOne(query)
+        res.send(result)
+      })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
